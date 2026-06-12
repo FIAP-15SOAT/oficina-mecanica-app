cfa4adb9607b9101ab50d2b79eaedf0cb9908b18
Lucas Almeida da Silva <lucas.almeida-silva@hotmail.com>
2026-06-12 09:57:55 -0300

    refactor: update WORK_ORDER_NUMBER_REGEX to use String.raw to fix sonar issue

--- src/domain/constants/regex/work-order-number.regex.ts
+++ src/domain/constants/regex/work-order-number.regex.ts
@@ -1,3 +1,5 @@
 import { WORK_ORDER_NUMBER_MIN_LENGTH } from '../validation/work-order.constants';
 
-export const WORK_ORDER_NUMBER_REGEX = new RegExp(`^\\d{${WORK_ORDER_NUMBER_MIN_LENGTH},}$`);
+export const WORK_ORDER_NUMBER_REGEX = new RegExp(
+  String.raw`^\d{${WORK_ORDER_NUMBER_MIN_LENGTH},}$`,
+);
